0b379726dcd5def2551e052c1b94dcf05fd08d90
EvonShift <teryaevivan@gmail.com>
2018-05-06 13:36:20 +1100

    Текстура tShot

---
+++ TextureManager.java
@@ -0,0 +1,26 @@
+package com.evon.slash;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+public class TextureManager {
+	static Texture tJoy;
+	static Texture tJoyBack;
+	static Texture tPlayer;
+	static Texture tShot;
+
+	public static void upload(){
+		tJoyBack = new Texture(Gdx.files.internal("JoyBack.png"));
+		tJoy = new Texture(Gdx.files.internal("JoyStick.png"));
+		tPlayer = new Texture(Gdx.files.internal("Player.png"));
+		tShot = new Texture(Gdx.files.internal("Shot.png"));
+	}
+
+	public static void dispose(){
+		tJoy.dispose();
+		tJoyBack.dispose();
+		tPlayer.dispose();
+		tShot.dispose();
+	}
+}
