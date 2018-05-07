31532798c5b0a76642d80384761974c2b1043aa6
EvonShift <teryaevivan@gmail.com>
2018-05-07 16:59:14 +1100

    Регистрирование попаданий по игроку и счет

--- Player.java
+++ Player.java
@@ -2,6 +2,7 @@ package com.evon.slash;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Player {
@@ -9,13 +10,18 @@ public class Player {
 	private static MoveJoystick moveJoystick;
 	private static RotationJoystick rotationJoystick;
 	private static Sprite sprite;
+	private static Circle hitBox;
+	private static int hp;
+	private static int score;
 
 	public static void create(){
+		hp = 100;
 		pos = new Vector2(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
 		moveJoystick = new MoveJoystick(240, 240);
 		rotationJoystick = new RotationJoystick(Gdx.graphics.getWidth() - 240, 240);
 		sprite = new Sprite(TextureManager.tPlayer);
 		sprite.setOrigin(22, 30);
+		hitBox = new Circle(pos, 32);
 	}
 
 	public static void setPos(float x, float y) {
@@ -29,11 +35,22 @@ public class Player {
 
 	public static void update(){
 		sprite.setPosition(pos.x, pos.y);
+		sprite.draw(Main.getBatch());
 		moveJoystick.update();
 		rotationJoystick.update();
 		for (int i = 0; i < rotationJoystick.getShots().length; i++)
 			rotationJoystick.getShots()[i].update();
-		sprite.draw(Main.getBatch());
+		hitBox.setPosition(pos.x + 22, pos.y + 30);
+		for (int i = 0; i < rotationJoystick.getShots().length; i++) {
+			if (hitBox.contains(rotationJoystick.getShots()[i].getCollisionPos().x, rotationJoystick.getShots()[i].getCollisionPos().y) && !rotationJoystick.getShots()[i].isPlayer() && rotationJoystick.getShots()[i].isActive()){
+				System.out.println("df");
+				rotationJoystick.getShots()[i].setActive(false);
+				if (hp > 0) hp -= 1;
+			}
+		}
+		if (hp <= 0){
+			sprite.setAlpha(0.2f);
+		}
 	}
 
 	public static RotationJoystick getRotationJoystick() {
@@ -46,5 +63,15 @@ public class Player {
 
 	public static Sprite getSprite() {
 		return sprite;
+	}
+
+	public static void addScore() {if (hp > 0) score++;}
+
+	public static int getScore() {
+		return score;
+	}
+
+	public static int getHp() {
+		return hp;
 	}
 }
--- Shot.java
+++ Shot.java
@@ -3,18 +3,33 @@ package com.evon.slash;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+
+import java.awt.geom.Line2D;
+
+import javafx.scene.shape.Line;
 
 public class Shot{
 	private Sprite sprite;
+	private Sprite ggg;
 	private Vector2 pos;
 	private float al;
+	private double distance;
+	private boolean player;
+	private boolean active;
+	private Vector2 collisionPos;
 	public Shot(){
 		pos = new Vector2(-1, -1);
+		collisionPos = new Vector2(0, 0);
 		sprite = new Sprite(TextureManager.tShot);
 	}
 
-	public void shot (float x, float y, float degress){
+	public void shot (float x, float y, float degress, boolean player){
+		this.player = player;
+		if (!player) sprite.setColor(1, 0, 0, 1);
+		else sprite.setColor(1, 1, 1, 1);
+		active = true;
 		al = 1;
 		pos.x = x;
 		pos.y = y;
@@ -23,16 +38,40 @@ public class Shot{
 	}
 
 	public void update(){
+		collisionPos.x = (float) DtP.toCartesian(distance + 48,(double) sprite.getRotation(), false) + Player.getPos().x;
+		collisionPos.y = (float) DtP.toCartesian(distance + 48,(double) sprite.getRotation(), true) + Player.getPos().y;
+		if (active) distance += 2000 * Gdx.graphics.getDeltaTime();
+		sprite.setSize((float) distance, 1);
 		sprite.setX(pos.x);
 		sprite.setY(pos.y);
 		sprite.setAlpha(al);
 		sprite.draw(Main.getBatch());
 		if (al < 0) al = 0;
 		if (al > 0) al -= Gdx.graphics.getDeltaTime();
-		if (al == 0){ pos.x = -1; pos.y = -1; }
+		if (al == 0){ pos.x = -1; pos.y = -1; distance = 0;}
 	}
 
 	public float getAl(){
 		return al;
+	}
+
+	public Vector2 getCollisionPos() {
+		return collisionPos;
+	}
+
+	public void setPlayer(boolean player) {
+		this.player = player;
+	}
+
+	public boolean isPlayer() {
+		return player;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
 	}
 }
