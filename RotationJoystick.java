8e04cb162286025c4a5199a13c3aeef04941ecaa
EvonShift <teryaevivan@gmail.com>
2018-05-07 16:58:25 +1100

    Убраны строки "System.out.print" использовавшиеся для исправления ошибок

--- RotationJoystick.java
+++ RotationJoystick.java
@@ -10,7 +10,7 @@ public class RotationJoystick extends Joystick  {
 		attackSpeed = new Timer(7);
 		attackSpeed.resume();
 		attackSpeed.setLoop(false);
-		shots = new Shot[90];
+		shots = new Shot[80];
 		for (int i = 0; i < shots.length; i++)
 			shots[i] = new Shot();
 	}
@@ -18,13 +18,12 @@ public class RotationJoystick extends Joystick  {
 	public void action(){
 		if (touchPos.x != 0 && touchPos.y != 0) {
 			Player.setSpriteRotatiion((float) DtP.toPolaris(touchPos.x, touchPos.y, false));
-			System.out.println(attackSpeed.getPosition());
 			if (!attackSpeed.isPlaying()) {
-				System.out.println("gg");
 				attackSpeed.restart();
+				for (int j = 0; j < 2; j++)
 				for (int i = 0; i < shots.length; i++)
 					if (shots[i].getAl() == 0) {
-						shots[i].shot(Player.getPos().x + 22, Player.getPos().y + 30, (float) DtP.toPolaris(touchPos.x, touchPos.y + Math.random()*20-10, false));
+						shots[i].shot(Player.getPos().x + 22, Player.getPos().y + 30, (float) DtP.toPolaris(touchPos.x, touchPos.y + Math.random()*16-8, false), true);
 						Player.setPos(Player.getPos().x + (float) DtP.toCartesian(10 * Math.random(), (float) DtP.toPolaris(touchPos.x, touchPos.y, false) + 180, false),
 								Player.getPos().y + (float) DtP.toCartesian(10 * Math.random(), (float) DtP.toPolaris(touchPos.x, touchPos.y, false) + 180, true));
 						break;
@@ -32,12 +31,6 @@ public class RotationJoystick extends Joystick  {
 			}
 		}
 		attackSpeed.update();
-	}
-
-
-
-	public Timer getAttackSpeed() {
-		return attackSpeed;
 	}
 
 	public Shot[] getShots() {
